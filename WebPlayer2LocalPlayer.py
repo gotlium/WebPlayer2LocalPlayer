1f3a0bafbb23967d55ce06805ddfa90f8b836ff9
GoTLiuM InSPiRiT <gotlium@gmail.com>
2015-01-06 20:54:27 +0300

    Update WebPlayer2LocalPlayer.py

--- WebPlayer2LocalPlayer.py
+++ WebPlayer2LocalPlayer.py
@@ -1,1 +1,98 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+import re
+
+from PyQt5 import QtCore, QtWebKitWidgets, QtWidgets, QtNetwork
+from PyQt5.QtWebKit import QWebSettings
+
+APP_NAME = "WebPlayer2LocalPlayer"
+VIDEO_EXT = ["mp4", "flv"]
+FORBIDDEN_EXT = ["gif"]
+FORBIDDEN_REGEXP = ["rdr.php.*", ".*edgecastcdn.*"]
+FORBIDDEN_URL = "http://localhost/forbidden/"
+OPEN_CMD = "open -a VLC '%s'"
+
+
+class QNetworkAccessManager(QtNetwork.QNetworkAccessManager):
+    def __init__(self, *args, **kwargs):
+        super(QNetworkAccessManager, self).__init__(*args, **kwargs)
+        self.request = None
+
+    def createRequest(self, operation, request, *args, **kwargs):
+        self.processRequest(operation=operation, request=request)
+        reply = QtNetwork.QNetworkAccessManager.createRequest(
+            self, operation, request, *args, **kwargs)
+        reply.error.connect(self.catchError)
+        return reply
+
+    def processRequest(self, operation, request):
+        if operation == self.GetOperation:
+            self.request = request
+
+            for regexp in FORBIDDEN_REGEXP:
+                if re.match(regexp, self.getUrl()):
+                    self.interruptRequest()
+
+            if self.getExt() in FORBIDDEN_EXT:
+                self.interruptRequest()
+
+            if self.getExt() in VIDEO_EXT:
+                self.openPlayer(self.getUrl())
+                self.interruptRequest()
+
+    def getExt(self):
+        return self.getUrl().split(".")[-1]
+
+    def getUrl(self):
+        return self.request.url().toString()
+
+    def interruptRequest(self):
+        self.request.setUrl(QtCore.QUrl(FORBIDDEN_URL))
+
+    @staticmethod
+    def openPlayer(url):
+        print "Url for open: ", url
+        os.system(OPEN_CMD % url)
+
+    @staticmethod
+    def catchError(eid):
+        # print 'Error %d:' % eid
+        pass
+
+
+class MainWindow(QtWebKitWidgets.QWebView):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.setWindowTitle(APP_NAME)
+        self.setPage(QtWebKitWidgets.QWebPage())
+        self.showWebView(self.getUrl())
+        self.showMaximized()
+
+    def showWebView(self, url):
+        self.page().setNetworkAccessManager(QNetworkAccessManager())
+        self.load(QtCore.QUrl(url))
+
+    @staticmethod
+    def getUrl():
+        if len(sys.argv) != 2:
+            print "Enter url as secondary argument"
+            sys.exit(-1)
+        return sys.argv[1]
+
+    @staticmethod
+    def setSettings():
+        settings = QWebSettings.globalSettings()
+        settings.setAttribute(QWebSettings.AutoLoadImages, True)
+        settings.setAttribute(QWebSettings.JavascriptCanOpenWindows, True)
+        settings.setAttribute(QWebSettings.PluginsEnabled, True)
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication([])
+    main = MainWindow()
+    main.setSettings()
+    main.show()
+    sys.exit(app.exec_())
